f59c3fa7c4c847a46504d94cc88347cf9a7a7c5f
Reisen <reisen@morphism.org>
2019-03-13 23:39:40 +0000

    Move all Haskell side stuff under src.

--- lib/Pixel/Services.hs
+++
@@ -1,9 +0,0 @@
-module Pixel.Services
-  ( module Services
-  )
-where
-
---------------------------------------------------------------------------------
-
-import Pixel.Services.Image  as Services
-import Pixel.Services.Static as Services
